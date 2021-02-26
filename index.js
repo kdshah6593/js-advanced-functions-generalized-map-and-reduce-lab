648a74f6b7ca8f790417db2fc0825f53c58dbbc9
Kunal <kdshah6593@gmail.com>
2021-02-26 18:05:23 -0500

    commit tree 62e2cd8a7301f719eab3ea6de9ae168352df713e

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // Add your functions here
+function map(sourceArray, func) {
+    let newArray = [];
+    for (let i = 0; i < sourceArray.length; i++) {
+        newArray.push(func(sourceArray[i]))
+    }
+    return newArray;
+}
+
+function reduce(sourceArray, func, startingValue) {
+    let total = (!!startingValue) ? startingValue : sourceArray[0];
+    let i = (!!startingValue) ? 0 : 1;
+
+    for (i; i < sourceArray.length; i++) {
+        total = func(sourceArray[i], total)
+    }
+    return total;
+}
